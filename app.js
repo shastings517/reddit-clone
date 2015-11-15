2075d79e9924ff2e8f057189ed522da4255722a6
Samuel Hastings <shastings517@gmail.com>
2015-11-14 16:55:56 -0800

    fixed linked post bug

--- app.js
+++ app.js
@@ -3,30 +3,56 @@ var app = angular.module("redditClone", []);
     // $scope.addPost.ngHide();
     
   
+    
+    $scope.post = {};
+    $scope.allPosts = [];
+    $scope.formVisible = false;
+    // $scope.button = false;
+
+    
+
+    // $scope.resetForm = function resetForm(){
+    //   $scope.addPost.$setPristine();
+    // };
+
+    $scope.newPost = function(post){
+      $scope.allPosts.push($scope.post);
+      $scope.post = {};
+      $scope.formVisible = !$scope.formVisible;
+      $scope.addPost.title.$touched = false;
+      $scope.addPost.author.$touched = false;
+      $scope.addPost.image.$touched = false;
+      $scope.addPost.description.$touched = false;
+      $scope.post.postRating = 0;
+
+
+      
+      // debugger
+    };
+    $scope.showForm = function(){
+      $scope.formVisible = !$scope.formVisible;
+    };
+  });
+
+  app.controller('PostController', function($scope){
+
     $scope.postRating = 0;
+    $scope.post.
+
 
     $scope.upRating = function(){
       $scope.postRating++;
+      debugger
     };
+
     $scope.downRating = function(){
       $scope.postRating--;
     };
 
-    $scope.mv = {};
-    $scope.allPosts = [];
-
-    $scope.newPost = function(){
-      $scope.allPosts.push($scope.mv);
-      $scope.mv = {};
-      // debugger
-    };
-2
-    // $scope.vote = function(){
-    //   if($scope.postRating < 0){
-    //     return 
-    //   }
-    // };
   });
+
+    
+  
 
 
   // var app = angular.module("signInApp", []);
