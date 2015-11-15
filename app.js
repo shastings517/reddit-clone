09dc11a2699cf61e469d619dba1b6ed0029477ac
Samuel Hastings <shastings517@gmail.com>
2015-11-15 13:50:18 -0800

    adding sort functionality. title and votes done.

--- app.js
+++ app.js
@@ -1,48 +1,43 @@
 var app = angular.module("redditClone", []);
   app.controller('EventController', function($scope, $filter){
     
-  
-    
     $scope.post = {};
+    $scope.sortReverse = false;
+    $scope.formVisible = false;
+    
+    // SEED DATA
     $scope.allPosts = [
       {
         author: "Sam",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores provident fuga, ab architecto odio! Architecto quo dolorem ab incidunt nisi, explicabo neque vitae nam aliquid, sint, voluptas eius eligendi dignissimos!",
         image: "http://images.vcpost.com/data/images/full/45699/deadpool.jpeg",
         postRating: 0,
-        title: "Deadpool",
-        id:0
+        title: "Deadpool"
       },  
       {
         author: "Sam",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae consectetur quibusdam sequi consequatur quasi suscipit ipsum obcaecati sunt, perspiciatis ducimus, nihil unde dignissimos animi iste earum a iusto tempore, nesciunt.",
         image: "http://www.theamazingspidermangame.com/etc/designs/atvi/amazing-spiderman-game/amazing-spiderman-game-2/images/features/img5.jpg",
         postRating: 0,
-        title: "Spiderman",
-        id:1
+        title: "Spiderman"
       },
       {
         author: "Sam",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae consectetur quibusdam sequi consequatur quasi suscipit ipsum obcaecati sunt, perspiciatis ducimus, nihil unde dignissimos animi iste earum a iusto tempore, nesciunt.",
         image: "https://i.ytimg.com/vi/FEZLYYeECtc/maxresdefault.jpg",
         postRating: 0,
-        title: "Batman",
-        id:2
+        title: "Batman"
       },
       {
         author: "Sam",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae consectetur quibusdam sequi consequatur quasi suscipit ipsum obcaecati sunt, perspiciatis ducimus, nihil unde dignissimos animi iste earum a iusto tempore, nesciunt.",
         image: "http://cdn.zmescience.com/wp-content/uploads/2013/07/superman-mkvsdc-1.jpg",
         postRating: 0,
-        title: "Superman",
-        id:3
+        title: "Superman"
       }    
-
-
     ];
-    $scope.formVisible = false;
-    
 
+    
     $scope.newPost = function(post){
       $scope.post.postRating = 0;
       // $scope.post.id = 0;
@@ -61,56 +56,25 @@ var app = angular.module("redditClone", []);
       $scope.formVisible = !$scope.formVisible;
     };
 
-    // $scope.upRating = function(item) {
-    //     var post = $scope.allPosts.filter(function(obj) {
-    //         return ($scope.obj.$$hashKey === $scope.item.$$hashKey);
-    //     });
-    //     post[0].postRating = post[0].postRating + 1;
-    //     // vm.sort();
-    // };
-  // });
-  // app.controller('PostController', function($scope, $filter){
+    
     $scope.upRating = function($$hashKey){
       var postArr = $scope.allPosts;
       for(var i = 0; i < postArr.length; i++){
         if(postArr[i].$$hashKey === $$hashKey){
           postArr[i].postRating++;
         }
-        // debugger
-      }
-      
-        
+      } 
     };
-    // $scope.removeContact = function(firstName){       
-    //       var index = -1;   
-    //       var comArr = $scope.contacts;
-    //       for( var i = 0; i < comArr.length; i++ ) {
-    //         if( comArr[i].firstName === firstName ) {
-    //           index = i;
-    //           break;
-    //         }
-    //       }
-    //       if( index === -1 ) {
-    //         alert( "Something gone wrong" );
-    //       }
-    //       $scope.contacts.splice( index, 1 );   
-    //   };
-
+    
     $scope.downRating = function($$hashKey){
       var postArr = $scope.allPosts;
       for(var i = 0; i < postArr.length; i++){
         if(postArr[i].$$hashKey === $$hashKey){
           postArr[i].postRating--;
         }
-        // debugger
       }
     };
 
-    // $scope.orderByVote = function(){
-
-    // };
-
-    $scope.sortType = 'postRating';
   });
 
 
